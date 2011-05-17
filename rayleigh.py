e819b4d557e0fffd4168ce49b398a2cf6e3b89b8
jordi <jordi@21538048-27be-4368-8349-8bf56fbb7052>
2011-05-17 16:55:57 +0000

    Modified figure according to computation
    git-svn-id: http://ruth.upc.es/svn/gemt/2010/flood@212 21538048-27be-4368-8349-8bf56fbb7052

--- rayleigh.py
+++ rayleigh.py
@@ -1,55 +1,63 @@
 from numpy import *
+import sys
 import matplotlib
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
+
+mode= 'save'
 
 # http://en.wikipedia.org/wiki/Rayleigh_distribution
 ray = lambda t,sigma=1: t>0 and t/sigma**2*exp(-t**2/(2*sigma**2)) or 0.
 
 dt = 1.
 s = tm = 10
-ts = linspace(-10,100,1000)
+a = -10
+b = 100
+ts = linspace(a,b,1000)
 qs = array([1 + 40*ray(t,tm) for t in ts])
+tqs = array([ts,qs]).transpose()
 qmax = max(qs)
 qmin = min(qs)
 tmax = ts[argmax(qs)]
-vs = cumsum(qs)
-qv = array([qs,vs]).transpose()
-qv1 = qv[ts<tmax]
-qv2 = qv[ts>=tmax]
-print "Qmax =",qmax, " at tmax =", tmax
+print "qmax =",qmax, " at tmax =", tmax
+tqs1 = tqs[ts<tmax,:]
+tqs2 = tqs[ts>tmax,:]
 
+assert all(diff(tqs1[:,1])>=0), "Not increasing"
+assert all(diff(tqs2[:,1])<=0), "Not decreasing"
 
+# Inverse interpolation to find t_{-+}(q)
 undef = NaN
+t1 = lambda q: interp(q, tqs1[:,1], tqs1[:,0],left=undef, right=undef)
+t2 = lambda q: interp(q, tqs2[::-1,1], tqs2[::-1,0], left=undef, right=undef)
 
-fg1 = plt.figure(1)
-ax1 = fg1.add_subplot(111)
-ax1.plot(ts,qs,'b-')
+ys = linspace(0,qmax,100)[:-1]
+dq = ys[1]-ys[0]
+dt = t2(ys)-t1(ys)
+#print dt
+#assert all(dt>=0), "Negative dt"
+#assert all(logical_or(isnan(diff(dt)), diff(dt)<=0)), "Non decreasing dt"
+vy = cumsum(dq*dt[::-1]) # Start at the top
 
+fg = plt.figure(1)
+ax1 = fg.add_subplot(121)
+ax2 = fg.add_subplot(122)
+ax1.plot(ys[::-1],vy,'b-')
 
-fg2 = plt.figure(2)
-ax2 = fg2.add_subplot(111)
-ax2.plot(qs,vs,'b-')
-qt = linspace(qmin,qmax,100)[:-1]
-assert all(diff(qv1[:,0])>=0), "Not increasing "
-assert all(diff(qv2[:,0])<=0), "Not decreasing"
-fv1 = lambda q: interp(q, qv1[:,0], qv1[:,1])
-fv2 = lambda q: interp(q, qv2[::-1,0], qv2[::-1,1],left=undef,right=undef) # interp requires increasing
-
-v1 = fv1(qt)
-v2 = fv2(qt)
-ax2.plot(qt, v2-v1, 'g-')
-
-W = 350.
-Qf = interp(W,(v2-v1)[::-1],qt[::-1],left=-undef,right=undef)
-print "Qf =", Qf
-ax2.plot([Qf,Qf],[0,fv2(Qf)],'r:', [Qf,Qf], [fv1(Qf),fv2(Qf)],'r-')
-ax1.plot(ts,len(ts)*[Qf],'r:')
+W = 22.
+qf = interp(W,vy,ys[::-1])
+print "W = ", W
+print "Qf = ", qf
+ax1.plot([1,qf,qf],[W,W,0],'g:')
+ax2.plot(ts,qs,'b-')
+ax2.plot([a,b],[qf,qf],'r:')
 fcolor = 'cyan'
-ax1.fill_between(ts,qs,Qf,where=qs>Qf,facecolor=fcolor,color=fcolor)
-ax1.axis([-10,100,0,5])
+ax2.fill_between(ts,qs,qf,where=qs>qf,facecolor=fcolor,color=fcolor)
+ax2.axis([a,b,0,5])
 
 
 
-fg1.savefig('qt.eps')
-fg2.savefig('vq.eps')
+if mode == 'save':
+	fg.savefig('cut.pdf')
+else:
+	plt.show()
